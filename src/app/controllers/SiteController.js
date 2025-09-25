09e129bc25f5b7094751fcce5d60e1e1e2524cc5
PurePeter <phatlenguyenthanh4@gmail.com>
2025-09-25 10:17:13 +0700

    add model, controller in App and fix little bit path in routes

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,22 @@
-class SiteController {
-  // GET /
-  index(req, res) {
-    res.render("home");
-  }
+const Course = require('../models/Course');
 
-  //[GET] /search
-  search(req, res) {
-    res.render("search");
-  }
+class SiteController {
+    // GET /
+    async index(req, res) {
+        try {
+            const courses = await Course.find({});
+            res.json(courses);
+        } catch (err) {
+            res.status(400).json({ error: 'ERROR!!!' });
+        }
+
+        // res.render('home');
+    }
+
+    //[GET] /search
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
